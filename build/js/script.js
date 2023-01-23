95ad2909b87349f93c9a35fd36d6cfa06935442a
Tato <naranjosa2004@gmail.com>
2023-01-23 09:57:46 -0500

    Fix Burguer Menu

--- build/js/script.js
+++ build/js/script.js
@@ -1,3 +1,2 @@
-
-
+function navegacionFija(){const e=document.querySelector(".burger"),n=document.querySelector(".header");window.addEventListener("scroll",(function(){console.log(n.getBoundingClientRect()),n.getBoundingClientRect().top<0?e.classList.add("hidden"):e.classList.remove("hidden")}))}navegacionFija();
 //# sourceMappingURL=script.js.map
